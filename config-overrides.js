6c9097667c88db9ef244f991f1ec538ba1eb3f98
felixparejapmdit <155054492+felixparejapmdit@users.noreply.github.com>
2025-01-22 10:21:09 +0800

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,21 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = function override(config) {
   config.resolve.fallback = {
     ...config.resolve.fallback,
     crypto: require.resolve("crypto-browserify"),
-    stream: require.resolve("stream-browserify"), // Add fallback for 'stream'
+    stream: require.resolve("stream-browserify"),
+    buffer: require.resolve("buffer"),
+    path: require.resolve("path-browserify"),
   };
+
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: "process/browser", // Polyfill for process
+      Buffer: ["buffer", "Buffer"], // Polyfill for Buffer
+    }),
+  ]);
+
   return config;
 };
