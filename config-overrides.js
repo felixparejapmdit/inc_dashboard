50cec9cd2c2d45b92bc56a72d86ef1d844c9fc17
felixparejapmdit <155054492+felixparejapmdit@users.noreply.github.com>
2025-01-22 10:46:05 +0800

    up

--- config-overrides.js
+++ config-overrides.js
@@ -7,13 +7,13 @@ module.exports = function override(config) {
     crypto: require.resolve("crypto-browserify"),
     stream: require.resolve("stream-browserify"),
     buffer: require.resolve("buffer"),
-    path: require.resolve("path-browserify"),
+    process: require.resolve("process/browser.js"), // Add .js extension
   };
 
   config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
-      process: "process/browser", // Polyfill for process
-      Buffer: ["buffer", "Buffer"], // Polyfill for Buffer
+      process: "process/browser.js", // Ensure .js extension
+      Buffer: ["buffer", "Buffer"],
     }),
   ]);
 
