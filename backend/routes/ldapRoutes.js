5860c07ee8e411122cc0169255f39e0121a5dbaf
felixparejapmdit <155054492+felixparejapmdit@users.noreply.github.com>
2025-05-16 14:51:02 +0800

    Update ldapRoutes.js

--- backend/routes/ldapRoutes.js
+++ backend/routes/ldapRoutes.js
@@ -3,21 +3,12 @@ const router = express.Router();
 
 const ldapController = require("../controllers/ldapController");
 
-const bcrypt = require("bcryptjs");
-const ldap = require("ldapjs");
-
-const fs = require("fs");
-const path = require("path");
-
-const LDAP_URL = process.env.LDAP_URL;
-const BIND_DN = process.env.BIND_DN;
-const BIND_PASSWORD = process.env.BIND_PASSWORD;
-const BASE_DN = process.env.BASE_DN;
-
-const auth = require("../middlewares/auth");
-
 // Define routes and associate them with controller functions
+
+router.get("/ldap/user/:username", ldapController.getUserByUsername);
+
 router.get("/api/test_ldap_connection", ldapController.testLdapConnection);
+
 router.get("/api/ldap/users_json", ldapController.getUsersFromJson);
 router.get("/api/ldap/users", ldapController.getLdapUsersAndGroups);
 router.get(
@@ -25,7 +16,6 @@ router.get(
   ldapController.authenticateUserFromJson
 );
 
-router.get("/ldap/user/:username", ldapController.getUserByUsername);
 //router.get("/ldap/user/:username", auth, ldapController.getUserByUsername);
 
 router.get("/ldap/groups", ldapController.getGroups);
