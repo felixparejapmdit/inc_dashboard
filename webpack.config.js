d05c1ecb538b3736c5327bcd79217df454a4ea10
felixparejapmdit <155054492+felixparejapmdit@users.noreply.github.com>
2025-09-01 15:51:35 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -78,7 +78,7 @@ module.exports = {
       http: require.resolve("stream-http"),
       https: require.resolve("https-browserify"),
       os: require.resolve("os-browserify/browser"),
-      url: require.resolve("url"),
+      url: require.resolve("url1"),
     },
   },
 };
